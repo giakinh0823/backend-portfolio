a52e43148034551ed8be82ca1e90adf34641a53f
Gia Kinh <giakinh2000@gmail.com>
2021-12-21 23:08:42 +0700

    Empty django app

--- backend/settings.py
+++ backend/settings.py
@@ -100,8 +100,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'CONN_MAX_AGE': 500,
     }
 }
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # DATABASES = {
 #     'default': {
