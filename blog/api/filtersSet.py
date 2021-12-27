1a432a79b0acbd6239079f6cc2282afe76186d90
Gia Kinh <giakinh2000@gmail.com>
2021-12-27 20:14:43 +0700

    done

--- blog/api/filtersSet.py
+++ blog/api/filtersSet.py
@@ -5,22 +5,23 @@ from blog.models import Blog, Topic, Photo, Tag
 class BlogFilter(django_filters.FilterSet):
     class Meta:
         model = Blog
-        fields = ['id','title', 'slug', 'created_at', 'updated_at', 'topics__name', 'tags__name','author__username']
+        fields = ['id', 'title', 'slug', 'created_at', 'updated_at',
+                  'topics', 'topics__name', 'tags', 'tags__name', 'author__username']
 
 
 class TopicFilter(django_filters.FilterSet):
     class Meta:
         model = Topic
         fields = ['name', 'slug', 'is_remove']
-        
+
 
 class PhotoFilter(django_filters.FilterSet):
     class Meta:
         model = Photo
         fields = ['id']
-        
+
+
 class TagFilter(django_filters.FilterSet):
     class Meta:
         model = Tag
         fields = ['name']
-        
